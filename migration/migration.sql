22d02dd5de08ec86e14b12fabc4316c96806ddcc
pranavshinde87 <pranavshinde87@gmail.com>
2019-01-20 19:44:35 +0530

    Update migration.sql

--- migration/migration.sql
+++ migration/migration.sql
@@ -4,3 +4,24 @@
 --checked in issue no: 02
 --checked in reason:
 
+CREATE TABLE Login (
+  id INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  username VARCHAR(100)  NOT NULL  ,
+  password_2 VARCHAR(25)  NOT NULL    ,
+PRIMARY KEY(id));
+
+
+CREATE TABLE Users (
+  id INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  UserId INTEGER UNSIGNED  NOT NULL  ,
+  UserType INTEGER UNSIGNED  NOT NULL    ,
+PRIMARY KEY(id, UserId, UserType)  ,
+INDEX Users_UserId_FK(UserId)  ,
+INDEX Users_UserType_FK(UserType));
+
+
+
+CREATE TABLE User Type (
+  id INTEGER UNSIGNED  NOT NULL   AUTO_INCREMENT,
+  User_Type_Name VARCHAR(100)  NULL    ,
+PRIMARY KEY(id));
